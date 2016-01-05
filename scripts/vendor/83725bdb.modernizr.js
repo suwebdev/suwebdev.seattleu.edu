4ba5cfb20ade57cf5169ba48e1b4daf50b9057cc
Shawn Rider <shawn@shawnrider.com>
2016-01-05 15:45:22 -0800

    Built suwebdev-github-io from commit 4721939 on branch develop
    Change-Id: Ie53c7b117e7cce31d2217e07d53979afd1286105

--- scripts/vendor/83725bdb.modernizr.js
+++ scripts/vendor/83725bdb.modernizr.js
@@ -1,4 +1,4 @@
 /* Modernizr (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-shiv-load-cssclasses-svg-input
  */
-;{"statusCode":404,"error":"Not Found"}{"statusCode":404,"error":"Not Found"};
+;;
